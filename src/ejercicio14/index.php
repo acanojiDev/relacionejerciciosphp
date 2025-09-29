fba5882fe40e41b85f4cbdea133ad56e3aafb202
acanojiDev <aantoniiocano@gmail.com>
2025-09-29 18:51:15 +0200

    ejercicio 14 changes

--- src/ejercicio14/index.php
+++ src/ejercicio14/index.php
@@ -6,52 +6,56 @@
     <title>Document</title>
     <link rel="stylesheet" href="../global.scss">
     <link rel="stylesheet" href="../style.css">
+    <style>
+        li{
+            color: white;
+        }
+    </style>
 </head>
 <body>
+<?php
+    $palos = ["Oros", "Copas", "Espadas", "Bastos"];
 
+    $puntos = [
+        "As" => 11,
+        "2"  => 0,
+        "3"  => 10,
+        "4"  => 0,
+        "5"  => 0,
+        "6"  => 0,
+        "7"  => 0,
+        "Sota" => 2,
+        "Caballo" => 3,
+        "Rey" => 4
+    ];
+
+    $figuras = array_keys($puntos);
+
+    $baraja = [];
+    foreach($palos as $palo){
+        foreach($figuras as $figura){
+            $baraja[] = "$figura de $palo";
+        }
+    }
+
+    shuffle($baraja);
+
+    $mano = array_slice($baraja, 0, 10);
+
+    $total = 0;
+    echo "<h2>Cartas seleccionadas:</h2><ul>";
+    foreach($mano as $carta){
+        echo "<li>$carta</li>";
+
+        $partes = explode(" de ", $carta);
+        $figura = $partes[0];
+
+        $total += $puntos[$figura];
+    }
+    echo "</ul>";
+
+    echo "<p><strong>Total de puntos:</strong> $total</p>";
+    ?>
 </body>
 </html>
 
-<?php
-$palos = ["Oros", "Copas", "Espadas", "Bastos"];
-
-$puntos = [
-    "As" => 11,
-    "2"  => 0,
-    "3"  => 10,
-    "4"  => 0,
-    "5"  => 0,
-    "6"  => 0,
-    "7"  => 0,
-    "Sota" => 2,
-    "Caballo" => 3,
-    "Rey" => 4
-];
-
-$figuras = array_keys($puntos);
-
-$baraja = [];
-foreach($palos as $palo){
-    foreach($figuras as $figura){
-        $baraja[] = "$figura de $palo";
-    }
-}
-
-shuffle($baraja);
-
-$mano = array_slice($baraja, 0, 10);
-
-$total = 0;
-echo "<h2>Cartas seleccionadas:</h2><ul>";
-foreach($mano as $carta){
-    echo "<li>$carta</li>";
-
-    $partes = explode(" de ", $carta);
-    $figura = $partes[0];
-
-    $total += $puntos[$figura];
-}
-echo "</ul>";
-
-echo "<p><strong>Total de puntos:</strong> $total</p>";
-?>
